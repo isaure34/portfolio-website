042ae4cd992c69c7ca6e01e71bddddf45fcdb605
isaure34 <isaure.ajarrista@gmail.com>
2021-05-12 11:49:33 -0400

    added js
    modified the nav page and added a scroll to top button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("Hello world")
+function menuToggle(){
+	var x = document.getElementById('myNavtoggle');
+	if(x.className === 'navtoggle'){
+		x.className += ' responsive';
+	} else{
+		x.className = 'navtoggle';
+	}
+}
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+	scrollFunction();
+}
+
+function scrollFunction() {
+	if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction () {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
